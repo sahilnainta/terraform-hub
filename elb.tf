a24804f7ba88437a27cc2453275c8cb2c754bada
Sahil Nainta <saah.shimla@gmail.com>
2021-11-29 18:49:52 +0530

    Created & Validated Cert via ACM and add to ELB

--- elb.tf
+++ elb.tf
@@ -10,6 +10,15 @@ resource "aws_elb" "app_elb" {
     instance_port     = "80"
     instance_protocol = "http"
   }
+  
+  listener {
+    lb_port           = 443
+    lb_protocol       = "https"
+    instance_port     = "80"
+    instance_protocol = "http"
+    ssl_certificate_id    = aws_acm_certificate_validation.cert.certificate_arn
+  }
+  
   health_check {
     healthy_threshold   = 2
     unhealthy_threshold = 2
--- route53.tf
+++ route53.tf
@@ -1,11 +1,20 @@
-data "aws_route53_zone" "app" {
-  name         = "32nd.com"
+data "aws_route53_zone" "hosted_zone" {
+  name         = var.app_hosted_dns
   private_zone = false
+}
+
+resource "aws_route53_record" "bastion" {
+  allow_overwrite = true
+  zone_id = data.aws_route53_zone.hosted_zone.zone_id
+  name    = format("%s.%s", var.bastion_host_prefix, var.app_hosted_dns)
+  type    = "A"
+  ttl     = "300"
+  records = [aws_instance.jump_box.public_ip]
 }
 
 resource "aws_route53_record" "api" {
   allow_overwrite = true
-  zone_id = data.aws_route53_zone.app.zone_id
+  zone_id = data.aws_route53_zone.hosted_zone.zone_id
   name    = format("%s.%s", var.app_dns_prefix, var.app_hosted_dns)
   type    = "A"
 
@@ -16,11 +25,38 @@ resource "aws_route53_record" "api" {
   }
 }
 
-resource "aws_route53_record" "bastion" {
+resource "aws_acm_certificate" "cert" {
+  domain_name       = format("%s.%s", var.app_dns_prefix, var.app_hosted_dns)
+  validation_method = "DNS"
+
+  tags = {
+    Project = var.project
+    Name    = format("cert_%s.%s", var.app_dns_prefix, var.app_hosted_dns)
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
   allow_overwrite = true
-  zone_id = data.aws_route53_zone.app.zone_id
-  name    = format("%s.%s", var.bastion_host_prefix, var.app_hosted_dns)
-  type    = "A"
-  ttl     = "300"
-  records = [aws_instance.jump_box.public_ip]
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.hosted_zone.zone_id
+}
+
+resource "aws_acm_certificate_validation" "cert" {
+  certificate_arn         = aws_acm_certificate.cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.validation : record.fqdn]
 }
