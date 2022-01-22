3365b1cc6305cdd029aa1cbfcefb7517fe500f9c
Sahil Nainta <saah.shimla@gmail.com>
2022-01-22 20:11:33 +0530

    code refactoring for faster uptime

--- prepare_ami.sh
+++ prepare_ami.sh
@@ -28,50 +28,6 @@ sudo yum -y install nginx
 
 sudo systemctl start nginx
 
-# prod checkout & setup
-cd /home/ec2-user
-mkdir prod
-cd /home/ec2-user/prod
-
-git clone --depth 1 -b master https://sahilnainta:sahil32nd@bitbucket.org/vikas_gh/hub-nodejs.git
-cd hub-nodejs
-cp .env.production .env
-
-yarn install
-
-# staging checkout & setup
-cd /home/ec2-user
-mkdir staging
-cd /home/ec2-user/staging
-
-git clone --depth 1 -b master https://sahilnainta:sahil32nd@bitbucket.org/vikas_gh/hub-nodejs.git
-cd hub-nodejs
-cp .env.staging .env
-
-yarn install
-
-# qa checkout & setup
-cd /home/ec2-user
-mkdir qa
-cd /home/ec2-user/qa
-
-git clone --depth 1 -b master https://sahilnainta:sahil32nd@bitbucket.org/vikas_gh/hub-nodejs.git
-cd hub-nodejs
-cp .env.qa .env
-
-yarn install
-
-# dev checkout & setup
-cd /home/ec2-user
-mkdir dev
-cd /home/ec2-user/dev
-
-git clone --depth 1 -b master https://sahilnainta:sahil32nd@bitbucket.org/vikas_gh/hub-nodejs.git
-cd hub-nodejs
-cp .env.dev .env
-
-yarn install
-
 # nginx configuration
 sudo touch /etc/nginx/conf.d/server.conf
 echo  'server {
@@ -112,20 +68,65 @@ sudo systemctl restart nginx
 # PM2 config
 yarn global add pm2
 
+# prod checkout & setup
+cd /home/ec2-user
+mkdir prod
+cd /home/ec2-user/prod
+
+git clone --depth 1 -b master https://sahilnainta:sahil32nd@bitbucket.org/vikas_gh/hub-nodejs.git
+cd hub-nodejs
+cp .env.production .env
+
+yarn install
+
 # start production PM2
 cd /home/ec2-user/prod/hub-nodejs
 PM2_HOME=/home/ec2-user/.pm2 pm2 start src/index.js -i max --node-args="-r esm" --wait-ready --name "prod-hub-graphql"
 pm2 save
+
+### staging checkout & setup
+cd /home/ec2-user
+mkdir staging
+cd /home/ec2-user/staging
+
+git clone --depth 1 -b master https://sahilnainta:sahil32nd@bitbucket.org/vikas_gh/hub-nodejs.git
+cd hub-nodejs
+cp .env.staging .env
+
+yarn install
 
 # start staging PM2
 cd /home/ec2-user/staging/hub-nodejs
 PM2_HOME=/home/ec2-user/.pm2 pm2 start src/index.js -i max --node-args="-r esm" --wait-ready --name "staging-hub-graphql"
 pm2 save
 
+
+### qa checkout & setup
+cd /home/ec2-user
+mkdir qa
+cd /home/ec2-user/qa
+
+git clone --depth 1 -b master https://sahilnainta:sahil32nd@bitbucket.org/vikas_gh/hub-nodejs.git
+cd hub-nodejs
+cp .env.qa .env
+
+yarn install
+
 # start qa PM2
 cd /home/ec2-user/qa/hub-nodejs
 PM2_HOME=/home/ec2-user/.pm2 pm2 start src/index.js -i max --node-args="-r esm" --wait-ready --name "qa-hub-graphql"
 pm2 save
+
+### dev checkout & setup
+cd /home/ec2-user
+mkdir dev
+cd /home/ec2-user/dev
+
+git clone --depth 1 -b master https://sahilnainta:sahil32nd@bitbucket.org/vikas_gh/hub-nodejs.git
+cd hub-nodejs
+cp .env.dev .env
+
+yarn install
 
 # start dev PM2
 cd /home/ec2-user/dev/hub-nodejs
