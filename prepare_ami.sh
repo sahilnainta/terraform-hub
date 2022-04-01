78a0759793f5b17c4b524f9fa0b958e6a51e9fd4
Sahil Nainta <saah.shimla@gmail.com>
2022-04-01 14:26:39 +0530

    added webSocket support

--- prepare_ami.sh
+++ prepare_ami.sh
@@ -41,6 +41,11 @@ echo  'server {
       proxy_set_header  X-Real-IP  $remote_addr;
       proxy_set_header  Host       $http_host;
       proxy_pass        http://127.0.0.1:6000;
+
+      # Websocket
+      proxy_http_version 1.1;
+      proxy_set_header Upgrade $http_upgrade;
+      proxy_set_header Connection "upgrade";
     }
 }
 server {
@@ -49,6 +54,11 @@ server {
       proxy_set_header  X-Real-IP  $remote_addr;
       proxy_set_header  Host       $http_host;
       proxy_pass        http://127.0.0.1:7000;
+      
+      # Websocket
+      proxy_http_version 1.1;
+      proxy_set_header Upgrade $http_upgrade;
+      proxy_set_header Connection "upgrade";
     }
 }
 server {
@@ -57,6 +67,11 @@ server {
       proxy_set_header  X-Real-IP  $remote_addr;
       proxy_set_header  Host       $http_host;
       proxy_pass        http://127.0.0.1:8000;
+
+      # Websocket
+      proxy_http_version 1.1;
+      proxy_set_header Upgrade $http_upgrade;
+      proxy_set_header Connection "upgrade";
     }
 }
 server {
@@ -65,6 +80,11 @@ server {
       proxy_set_header  X-Real-IP  $remote_addr;
       proxy_set_header  Host       $http_host;
       proxy_pass        http://127.0.0.1:5000;
+
+      # Websocket
+      proxy_http_version 1.1;
+      proxy_set_header Upgrade $http_upgrade;
+      proxy_set_header Connection "upgrade";
     }
 }' | sudo tee /etc/nginx/conf.d/server.conf
 
