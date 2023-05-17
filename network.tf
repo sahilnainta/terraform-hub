141194434becca1b3171e3189bb396bfd5cb29eb
Sahil Nainta <saah.shimla@gmail.com>
2023-05-17 14:56:42 +0530

    Reverted removal of EIP in NAT Gateway

--- network.tf
+++ network.tf
@@ -83,21 +83,21 @@ resource "aws_route_table_association" "rt_assoc" {
 
 # Create Elastic IP
 
-# resource "aws_eip" "nat_eip" {
-#   vpc   = true
-#   count = length(data.aws_availability_zones.available.names)
+resource "aws_eip" "nat_eip" {
+  vpc   = true
+  count = length(data.aws_availability_zones.available.names)
 
-#   tags = {
-#     Name    = "${format("%s-nat-eip-%03d", var.project, count.index + 1)}"
-#     Project = var.project
-#   }
-# }
+  tags = {
+    Name    = "${format("%s-nat-eip-%03d", var.project, count.index + 1)}"
+    Project = var.project
+  }
+}
 
 # Create NAT Gateway for prv_sub
 resource "aws_nat_gateway" "ngw" {
   count         = length(data.aws_availability_zones.available.names)
-  # allocation_id = aws_eip.nat_eip[count.index].id
-  connectivity_type = "private"
+  allocation_id = aws_eip.nat_eip[count.index].id
+  connectivity_type = "public"
   subnet_id     = element(aws_subnet.pub_sub.*.id, count.index)
 
   tags = {
