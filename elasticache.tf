ffabde869489ab743b78aa26b33f2ed487d61fbd
Sahil Nainta <saah.shimla@gmail.com>
2023-07-05 12:51:28 +0530

    Updated Redis version to 5.0.6

--- elasticache.tf
+++ elasticache.tf
@@ -13,8 +13,8 @@ resource "aws_elasticache_cluster" "redis" {
   engine               = "redis"
   node_type            = "cache.t3.micro"
   num_cache_nodes      = 1
-  parameter_group_name = "default.redis3.2"
-  engine_version       = "3.2.10"
+  parameter_group_name = "default.redis5.0"
+  engine_version       = "5.0.6"
   port                 = 6379
 
   subnet_group_name   = "${aws_elasticache_subnet_group.redis_subnet.name}"
