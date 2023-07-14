bd2bcedb14fc95e2e06ab51bb7b34499e1220e02
Sahil Nainta <saah.shimla@gmail.com>
2023-07-14 13:30:01 +0530

    Upgraded Redis version to 7.0

--- elasticache.tf
+++ elasticache.tf
@@ -13,8 +13,8 @@ resource "aws_elasticache_cluster" "redis" {
   engine               = "redis"
   node_type            = "cache.t3.micro"
   num_cache_nodes      = 1
-  parameter_group_name = "default.redis5.0"
-  engine_version       = "5.0.6"
+  parameter_group_name = "default.redis7"
+  engine_version       = "7.0"
   port                 = 6379
 
   subnet_group_name   = "${aws_elasticache_subnet_group.redis_subnet.name}"
